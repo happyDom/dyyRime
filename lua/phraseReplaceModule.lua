25b92e6af0be3dfee7486ecde3a55d8dd7984118
dyyRime <douyaoyuan@126.com>
2024-01-07 16:33:14 +0800

    敏感词过滤 完成配置

---
+++ lua/phraseReplaceModule.lua
@@ -0,0 +1,123 @@
+-- phraseReplaceModule.lua
+-- Copyright (C) 2023 yaoyuan.dou <douyaoyuan@126.com>
+
+local M={}
+local dict={}
+local dbgFlg = false
+
+--引入系统变更处理模块
+local ok, sysInfoRes = pcall(require, 'sysInfo')
+local currentDir = sysInfoRes.currentDir
+local userName = sysInfoRes.userName
+
+--设置 dbg 开关
+local function setDbg(flg)
+	dbgFlg = flg
+	sysInfoRes.setDbg(flg)
+	
+	print('phraseReplace dbgFlg is '..tostring(dbgFlg))
+end
+
+--将文档处理成行数组
+local function files_to_lines(...)
+	if dbgFlg then
+		print("--->files_to_lines called here")
+	end
+	local tab=setmetatable({},{__index=table})
+	local index=1
+	for i,filename in next,{...} do
+		local fn = io.open(filename)
+		if fn then
+			for line in fn:lines() do
+				if not line or #line > 0 then
+					tab:insert(line)
+				end
+			end
+			fn:close()
+		end
+	end
+	
+	if dbgFlg then
+		print("--->files_to_lines completed here")
+	end
+	return tab
+end
+
+local function dictload(...) -- filename)
+	if dbgFlg then
+		print("-->dictload called here")
+	end
+	
+	local lines=files_to_lines(...)
+	local thisDict={}
+	
+	for i,line in next ,lines do
+		if not line:match("^%s*#") then  -- 第一字 # 为注释行
+			local key,val = string.match(line,"(.+)\t(.+)")
+			if nil == key then
+				key = string.match(line,'(%S+)')
+				val = ''
+			end
+			if nil ~= key then
+				--此处，如果key 已经存在，则使用后来的值顶替旧的值
+				thisDict[key] = val
+			end
+		end
+	end
+	
+	if dbgFlg then
+		print("-->dictload completed here")
+	end
+	return thisDict
+end
+
+--===========================test========================
+local function test(printPrefix)
+	if nil == printPrefix then
+		printPrefix = ' '
+	end
+	if dbgFlg then
+		print('phraseReplace test starting...')
+		
+		sysInfoRes.test(printPrefix..' ')
+		
+		for k,v in pairs(dict) do
+			if dbgFlg then
+				print(printPrefix..k..'\t'..v)
+			end
+		end
+	end
+end
+
+--获取字典中的phrase
+local function getShownPhrase(k)
+	if nil == k then
+		return ''
+	elseif '' == k then
+		return ''
+	end
+	
+	--尝试获取 dictPhraseList 中 k 的列表
+	return dict[k]
+end
+
+function M.init(...)
+	local files={...}
+	--文件名不支持中文，其中 # 开始的行为注释行
+	table.insert(files,"phraseReplace.txt")
+	
+	for i,v in next, files do
+		files[i] = currentDir().."/".. v
+	end
+	dict= dictload(table.unpack(files))
+	
+	--抛出功能函数
+	M.getShownPhrase = getShownPhrase
+	M.userName = userName
+	M.setDbg = setDbg
+	M.test = test
+end
+
+M.init()
+
+return M
---
+++ lua/phraseReplace_Filter.lua
@@ -0,0 +1,60 @@
+-- phraseReplace_Filter.lua
+-- Copyright (C) 2023 yaoyuan.dou <douyaoyuan@126.com>
+--[[
+这个过滤器的主要作用是，对于候选项中命中的选项(OR 内容)，用其指定的内容来代替，如果没有指定，则使用 * 替换
+由于这个过滤器会改变候选项的内容（主要是会减少候选项数量），所以请将这个过滤器放在其它过滤器的最前端使用
+]]
+local phraseShown = ''
+
+local ok, res = pcall(require, 'phraseReplaceModule')
+local getShownPhrase = res.getShownPhrase
+
+--最长的comment长度限制
+local maxLenOfComment = 250
+
+local function phraseReplace_Filter(input, env)
+	--获取选项敏感词替换开关状态
+	local on = env.engine.context:get_option("phraseReplace")
+	local candsHasBeenHidden = {}
+	local candStart,candEnd
+	
+	for cand in input:iter() do
+		candStart = cand.start
+		candEnd = cand._end
+		
+		local candTxt = cand.text:gsub("%s","") or ""
+		
+		phraseShown = getShownPhrase(candTxt)
+		
+		if nil ~= phraseShown then
+			--不管是否开启选项替换，如果该选项是被命中的替换项，则加上替换标记 👙
+			cand.comment = '👙'..cand.comment
+			if '' ~= phraseShown then
+				if on then
+					--如果开启了选项替换功能，且存在替换内容
+					yield(Candidate("word", cand.start, cand._end, phraseShown, cand.comment))
+				else
+					--如果未开启选项替换功能
+					yield(cand)
+				end
+			else
+				if on then
+					--如果开启了选项替换功能，且这个选项应该被隐藏
+					table.insert(candsHasBeenHidden,candTxt)
+				else
+					--如果未开启选项替换功能
+					yield(cand)
+				end
+			end
+		else
+			yield(cand)
+		end
+	end
+	
+	--如果有被隐藏的选项，则抛出一个 * 选项提示
+	if 0 < #candsHasBeenHidden then
+		yield(Candidate("word", candEnd-1, candEnd, '*', '👙'))
+	end
+end
+
+return phraseReplace_Filter
