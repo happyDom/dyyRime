bb1c658f6d3c806c5e4593c026a21504ddf0f79d
dyyRime <douyaoyuan@126.com>
2024-01-11 13:22:40 +0800

    扩展词滤镜 完成配置

---
+++ lua/phraseExt_Filter.lua
@@ -0,0 +1,113 @@
+-- myPhrase_Filter.lua
+-- Copyright (C) 2023 yaoyuan.dou <douyaoyuan@126.com>
+local dbgFlg = false
+
+local ok, res = pcall(require, 'phraseExt_Module')
+local getPhraseList = res.getPhraseList
+
+--最长的comment长度限制
+local maxLenOfComment = 250
+
+--设置 dbg 开关
+local function setDbg(dbgFlg)
+	res.setDbg(dbgFlg)
+end
+
+--过滤器
+local function myPhrase_Filter(input, env)
+	--获取选项增强开关状态
+	local on = env.engine.context:get_option("phraseExt")
+	--获取应用程序标记状态[由于飞书暂不支持文本转表情的输入，帮使用 and false 将其关闭]
+	local feishuFlg = env.engine.context:get_option("feishuFlg") and false
+	local wechatFlg = env.engine.context:get_option("wechatFlg")
+	local qqFlg = env.engine.context:get_option("qqFlg")
+	local dingdingFlg = env.engine.context:get_option("dingdingFlg")
+	local minttyFlg = env.engine.context:get_option("minttyFlg")
+	local cmdFlg = env.engine.context:get_option("cmdFlg")
+	local pycharmFlg = env.engine.context:get_option("pycharmFlg")
+	local vscodeFlg = env.engine.context:get_option("vscodeFlg")
+	local matchedTxt = ''
+	local esType = ''
+	local esTxt = ''
+	
+	local cands={}
+	local thisTxt
+	
+	for cand in input:iter() do
+		--提交默认选项
+		if nil == cands[cand.text] then
+			yield(cand)
+			cands[cand.text]=true
+		end
+		if on then
+			local candTxt = cand.text:gsub("%s","") or ""
+			
+			if candTxt ~= "" then
+				--获取增强选项
+				local phraseList = getPhraseList(candTxt)
+				if #phraseList > 0 then
+					local idx
+					for idx=1,#phraseList do
+						thisTxt=phraseList[idx]
+						
+						if nil == cands[thisTxt] then
+							cands[thisTxt]=true
+							
+							esType,esTxt = string.match(thisTxt,"^es(.+)(%[.+%])$")
+							if nil ~= esType then
+								esType = string.lower(esType)
+
+								--这是一个表情选项
+								if feishuFlg and nil ~= string.find(esType,'fs') then
+									--这是一个 feishu 表情，且当前在 feishu 中输入
+									if nil ~= esTxt then
+										yield(Candidate("word", cand.start, cand._end, esTxt, '😃'))
+									end
+								elseif wechatFlg and nil ~= string.find(esType,'wx') then
+									--这是一个 wechat 表情，且当前在 wechat 中输入
+									if nil ~= esTxt then
+										yield(Candidate("word", cand.start, cand._end, esTxt, '😃'))
+									end
+								elseif qqFlg and nil ~= string.find(esType,'qq') then
+									--这是一个 QQ 表情，且当前在 QQ 中输入
+									if nil ~= esTxt then
+										yield(Candidate("word", cand.start, cand._end, esTxt, '😃'))
+									end
+								elseif dingdingFlg and nil ~= string.find(esType,'dt') then
+									--这是一个 dingtalk 表情，且当前在 钉钉 中输入
+									if nil ~= esTxt then
+										yield(Candidate("word", cand.start, cand._end, esTxt, '😃'))
+									end
+								end
+							else
+								--这不是一个表情选项
+								if string.lower(string.sub(thisTxt, 1, 4)) == 'git-' then
+									-- 这是一个以 git 开头的选项，这被认为是一个 git 命令
+									if minttyFlg or cmdFlg then
+										-- 修剪选项
+										thisTxt = string.sub(thisTxt, 5)
+
+										-- git 命令选项只在 cmd 窗口或者是 mitty 窗口才允许输出，以避免造成干扰
+										yield(Candidate("word", cand.start, cand._end, thisTxt:gsub("&nbsp"," "), '💡'))
+									end
+								elseif string.lower(string.sub(thisTxt, 1, 3)) == 'py-' then
+									-- 这是一个以 py- 开头的选项，这被认为是一个 python 关键字
+									if pycharmFlg or vscodeFlg then
+										-- 修剪选项
+										thisTxt = string.sub(thisTxt, 4)
+										-- python 关键字选项只在 pycharm 或者 vscode 中才允许输出， 以避免造成干扰
+										yield(Candidate("word", cand.start, cand._end, thisTxt:gsub("&nbsp"," "), '💡'))
+									end
+								else
+									yield(Candidate("word", cand.start, cand._end, thisTxt:gsub("&nbsp"," "), '💡'))
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+return myPhrase_Filter
---
+++ lua/phraseExt_Module.lua
@@ -0,0 +1,171 @@
+-- myPhrase.lua
+-- Copyright (C) 2023 yaoyuan.dou <douyaoyuan@126.com>
+local M={}
+local dict={}
+local dictPhraseList={}
+local dbgFlg = false
+
+--引入系统变更处理模块
+local ok, sysInfoRes = pcall(require, 'sysInfo')
+local currentDir = sysInfoRes.currentDir
+
+--设置 dbg 开关
+local function setDbg(flg)
+	dbgFlg = flg
+	sysInfoRes.setDbg(flg)
+	
+	print('myPhrase dbgFlg is '..tostring(dbgFlg))
+end
+
+--将这附串拆散成 table
+local function stringSplit(str,sp,sp1)
+	sp=(type(sp)=="string") and sp or " "
+	if 0==#sp then
+		sp="([%z\1-\127\194-\244][\128-\191]*)"
+	elseif 1==#sp then
+		sp="[^"..(sp=="%" and "%%" or sp).."]*"
+	else
+		sp1=sp1 or "^"
+		str=str:gsub(sp,sp1)
+		sp="[^"..sp1.."]*"
+	end
+	
+	local tab={}
+	for v in str:gmatch(sp) do
+		if ''~=v then
+			table.insert(tab,v)
+		end
+	end
+	return tab
+end
+
+--将文档处理成行数组
+local function files_to_lines(...)
+	if dbgFlg then
+		print("--->files_to_lines called here")
+	end
+	local tab=setmetatable({},{__index=table})
+	local index=1
+	for i,filename in next,{...} do
+		local fn = io.open(filename)
+		if fn then
+			for line in fn:lines() do
+				if not line or #line > 0 then
+					tab:insert(line)
+				end
+			end
+			fn:close()
+		end
+	end
+	
+	if dbgFlg then
+		print("--->files_to_lines completed here")
+	end
+	return tab
+end
+
+local function dictload(...) -- filename)
+	if dbgFlg then
+		print("-->dictload called here")
+	end
+	
+	local lines=files_to_lines(...)
+	local thisDict={}
+	
+	for i,line in next ,lines do
+		if not line:match("^%s*#") then  -- 第一字 # 为注释行
+			local keys,val = string.match(line,"(.+)\t(%C+)")
+			if nil ~= keys then
+				--尝试对关键字进行空格分割
+				local keyList = stringSplit(keys,' ')
+				local key=''
+				for idx=1,#keyList do
+					key = keyList[idx]
+					if nil ~= thisDict[key] then
+						--如果该key已经存在，追加在后面，注意加一个空格
+						thisDict[key] = thisDict[key]..' '..val
+					else
+						thisDict[key] = val
+					end
+				end
+			end
+		end
+	end
+	
+	if dbgFlg then
+		print("-->dictload completed here")
+	end
+	return thisDict
+end
+
+--===========================test========================
+local function test(printPrefix)
+	if nil == printPrefix then
+		printPrefix = ' '
+	end
+	
+	if dbgFlg then
+		print('myPhrase test starting...')
+	end
+	sysInfoRes.test(printPrefix..' ')
+	
+	for k,v in pairs(dict) do
+		if dbgFlg then
+			print(printPrefix..k..'\t'..v)
+		end
+	end
+end
+
+--获取字典中的phraseList
+local function getPhraseList(k)
+	if nil == k then
+		return {}
+	elseif '' == k then
+		return {}
+	end
+	
+	--尝试获取 dictPhraseList 中 k 的列表
+	local phraseList = dictPhraseList[k]
+	if nil == phraseList then
+		--phraseList 获取失败，尝试获取 dict 中 k 的字符串
+		local thisPhrase = dict[k]
+		if nil == thisPhrase then
+			--这个 k 在dict中不存在
+			phraseList={}
+		elseif thisPhrase == '' then
+			--这个 k 在dict中是空的
+			phraseList={}
+		else
+			--将获取的 thisPhrase 序列化到 dictPhraseList 中
+			dictPhraseList[k]=stringSplit(thisPhrase,' ')
+			
+			--再次从 dictPhraseList 中获取 k 的序列
+			phraseList = dictPhraseList[k]
+		end
+	end
+	
+	return phraseList
+end
+
+function M.init(...)
+	local files={...}
+	--文件名不支持中文，其中 # 开始的行为注释行
+	table.insert(files,"phraseExt commonPhrase.txt")
+	table.insert(files,"phraseExt esAppEmoji.txt")
+	table.insert(files,"phraseExt esUnicode.txt")
+	table.insert(files,"phraseExt personal.txt")
+	
+	for i,v in next, files do
+		files[i] = currentDir().."/".. v
+	end
+	dict= dictload(table.unpack(files))
+	
+	--抛出功能函数
+	M.getPhraseList = getPhraseList
+	M.setDbg = setDbg
+	M.test = test
+end
+
+M.init()
+
+return M
